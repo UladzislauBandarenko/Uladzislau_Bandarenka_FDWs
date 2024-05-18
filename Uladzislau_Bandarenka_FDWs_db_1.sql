dff9dee00104407782b5d9b0c25cf08a985ebbe3
UladzislauBandarenko <135656982+UladzislauBandarenko@users.noreply.github.com>
2024-05-18 16:51:02 +0300

    Add files via upload

--- Uladzislau_Bandarenka_FDWs_db_1.sql
+++ Uladzislau_Bandarenka_FDWs_db_1.sql
@@ -24,13 +24,13 @@ options (schema_name 'public', table_name 'remote_table');
 select * from local_remote_table;
 
 -- insert a new record into the foreign table
-insert into local_remote_table (id, name, age) values (4, 'maksim talkachou', 30);
+insert into local_remote_table (id, name, age) values (4, 'Uladzislau badarenka', 20);
 
 -- update an existing record in the foreign table
-update local_remote_table set age = 40 where name = 'darya korbut';
+update local_remote_table set age = 20 where name = 'mikita dubrovin';
 
 -- delete a record from the foreign table
-delete from local_remote_table where name = 'lizaveta piskunova';
+delete from local_remote_table where name = 'taras rybirn';
 
 create table local_table (
     id serial primary key,
@@ -40,9 +40,9 @@ create table local_table (
 
 -- the student has performed some operations using their own queries
 insert into local_table (name, email) values
-    ('darya korbut',  'darya.korbut@example.com'),
-    ('dmitry pytaylo','dmitry.pytaylo@example.com'),
-    ('lizaveta piskunova','lizaveta.piskunova@example.com');
+    ('bandarenka', 'uladzislau', 'bandarenka.uladzislau@student.ehu.lt'),
+    ('mikita', 'dubrovin', 'mikita.dubrovin@student.ehu.lt'),
+    ('taras', 'rybirn', 'taras.rybirn@student.ehu.lt');
 
 select r.*, l.email
 from local_remote_table as r 
